da26c5eb11917f8dd1834e9fe4f79de45fc12fa3
Samandar Sariboyev <samandarsariboyev69@gmail.com>
2024-02-05 09:32:02 +0500

    bux fixed

--- resources/views/cashier/payments.blade.php
+++ resources/views/cashier/payments.blade.php
@@ -472,6 +472,23 @@
                                 <td>${payment.date}</td>
                                 <td>${formattedMonth}</td>
                                 ${typeMoney}
+                                 <td>
+                                    <button payment_id="${payment.id}" subject="${payment.classes.name}"
+                                            amount="${formattedAmount}"
+                                            month="${formattedMonth}" name="${payment.student.name}"
+                                            date="${payment.date}" class="btn btn-outline-primary chek-button ">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                             viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                             stroke-linecap="round" stroke-linejoin="round"
+                                             class="feather feather-printer align-middle me-2">
+                                            <polyline points="6 9 6 2 18 2 18 9"></polyline>
+                                            <path
+                                                d="M6 18H4a2 2 0 0 1-2-2v-5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2h-2"></path>
+                                            <rect x="6" y="14" width="12" height="8"></rect>
+                                        </svg>
+                                        Chek
+                                    </button>
+                                </td>
                             </tr>
                         `;
                         tableBody.append(newRow);
